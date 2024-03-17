e5d484dc742b093d836e65bb20b365aa55cf3cf5
NhanDoV <60571509+NhanDoV@users.noreply.github.com>
2024-03-17 04:06:23 +0000

    sync

---
+++ Torch/CNN_models/run_simple_Neural_Network.py
@@ -0,0 +1,108 @@
+import torch
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+#====================== Load data: example iris ============================
+iris = sns.load_dataset("iris")
+lab = LabelEncoder()
+iris['species'] = lab.fit_transform(iris['species'])
+X = iris.drop(columns='species')
+y = iris['species']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, 
+                                                    stratify=y, random_state=42)
+X_train = torch.FloatTensor(X_train.to_numpy())
+X_test = torch.FloatTensor(X_test.to_numpy())
+y_train = torch.LongTensor(y_train.to_numpy())
+y_test = torch.LongTensor(y_test.to_numpy())
+
+#======================= modeling
+class DNN(torch.nn.Module):
+    """
+        Các tham số giữa các layers trong đây có thể được tùy chỉnh
+        Ở đây ta dùng activation-function là RELU
+        Layer 1 có dimension = 4 x 256
+        Layer 2 là 256 x 64
+        Output layer là 64 x 3
+        Ở đây ta đang giả định output_dim = 3 là số categories trong iris-data
+            - setosa
+            - versicolor    
+            - virginica     
+    """
+    def __init__(self, input_dim, output_dim):
+        super(CNN,self).__init__()
+        self.input_layer    = torch.nn.Linear(input_dim, 256)
+        self.hidden_layer1  = torch.nn.Linear(256, 64)
+        self.output_layer   = torch.nn.Linear(64, output_dim)
+        self.relu = torch.nn.ReLU()    
+    
+    def forward(self,x):
+        out =  self.relu(self.input_layer(x))
+        out =  self.relu(self.hidden_layer1(out))
+        out =  self.output_layer(out)
+        return out
+#====================== TRAINING    
+def train(model,optimizer,criterion,
+                  X_train,y_train,
+                  X_test,y_test,
+                  num_epochs,
+                  train_losses,test_losses,
+                  saved_model_name):
+    """
+        model: Your CNN model to classify
+        optimizer: thường là Adam / SGD
+        criterion: Cross-entropy
+        X_train, X_test, y_train, y_test: validation check
+        num_epochs: number of epochs
+        train_losses,test_losses: loss at each epoch
+        saved_model_name: name of model after trained
+    """
+    for epoch in range(num_epochs):
+        #clear out the gradients from the last step loss.backward()
+        optimizer.zero_grad()
+        
+        #forward feed
+        output_train = model(X_train)
+
+        #calculate the loss
+        loss_train = criterion(output_train, y_train)
+
+        #backward propagation: calculate gradients
+        loss_train.backward()
+
+        #update the weights
+        optimizer.step()
+        
+        output_test = model(X_test)
+        loss_test = criterion(output_test,y_test)
+
+        train_losses[epoch] = loss_train.item()
+        test_losses[epoch] = loss_test.item()
+
+        if (epoch + 1) % 50 == 0:
+            print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {loss_train.item():.4f}, Test Loss: {loss_test.item():.4f}")
+
+    torch.save(model.state_dict(), "clf_model.h5")
+#=======================================================================================
+if __name__ == "__main__":
+    num_epochs = int(input("Input the number of epochs: "))   
+    saved_model_name = input("Name to saved your model: ")
+    input_dim = int(input("Number of features in your models: "))
+    output_dim = int(input("Your output-dimension (how many categories in your output) is: "))
+    learning_rate = float(input("Your learning_rate is: "))
+    model = DNN(input_dim,output_dim)
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+    train_losses = np.zeros(num_epochs)
+    test_losses  = np.zeros(num_epochs)
+    print(100*"=")
+    train(model,optimizer,criterion,
+          X_train,y_train,X_test,y_test,
+          num_epochs,
+          train_losses,test_losses,
+          saved_model_name)
+    
+    # if you wanna to load model
+    # model = DNN(4, 3)
+    # model.load_state_dict(torch.load("clf_model.h5"))
--- Torch/Regression/linear_reg.py
+++ Torch/Regression/linear_reg.py
@@ -1,1 +1,108 @@
+import numpy as np
 import torch
+
+# Load your input values as numpy-array
+X = np.array([[1,2], [2,3], [3,3]]) #torch.Tensor([[1.0], [2.0], [3.0]])
+Y = np.array([5, 8,10]) #torch.Tensor([[5.0], [8.0], [10.0]])
+# ------------------------- in fact
+"""
+data_dir = {}
+data = pd.read_csv(data_dir)
+X = data[[input_cols]].to_numpy()
+Y = data[target_col].to_numpy()
+"""
+#========================== class of linear regression
+class LinearRegressionModel(torch.nn.Module):
+    """
+        Class này được sử dụng cho các bài toán Linear Regression trong torch
+    """
+    def __init__(self, input_dim, output_dim):
+        super(LinearRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, output_dim)  
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+def ridge_loss(Y_true, Y_pred, w, lamb):
+    """
+        Trong trường hợp bài toán hồi quy tuyến tính có một lượng chỉnh hóa bởi Ridge
+    """
+    pred_loss = torch.norm((Y_true-Y_pred),p='fro')**2
+    reg = torch.norm(w, p='fro')**2
+    return ((1/Y_true.size()[0])*pred_loss + lamb*reg)
+
+#============================== Convert numpy array to torch Variable
+# assume that we have 2 features as inputs
+inputs = torch.Tensor(X.reshape(-1, 2))
+print(inputs)
+labels = torch.Tensor(Y.reshape(-1, 1))
+print(labels)
+
+#============================== cpu or gpu
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+inputs = inputs.to(device)
+labels = labels.to(device)
+model = LinearRegressionModel(inputs.shape[1], 1)
+model.to(device)
+print('Device.training on cpu/gpu:', device)
+
+#============================= Other parameters
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=5e-2)
+
+print(f"|{'Epoch': ^5}|{'slopes': ^25}|{'intercept': ^10}|{'loss': ^15}|")
+# ============================= TRAINING
+def save_model(model, inputs, labels, criterion, optimizer, 
+               n_epochs=600, if_ridge=False, saved_model_name='reg_model.h5'):
+    """
+        Input params
+            * model (class)
+            * inputs: X
+            * labels: y
+            * criterion: MSE / MAE or MAPE
+            * optimizer = SGF / Adam / ...
+            * n_epochs : default 600
+            * if_ridge : if linear regression model included Ridge
+    """
+    for epoch in range(n_epochs): 
+        # Forward pass: Compute predicted y by passing 
+        # x to the model
+        pred_y = model(inputs)
+
+        # Compute and print loss
+        if if_ridge == False:
+            loss = criterion(pred_y, labels)
+        else:
+            loss = ridge_loss(pred_y, labels,
+                              model.state_dict()['linear.weight'],
+                              lamb=0.05) 
+
+        # Zero gradients, perform a backward pass, 
+        # and update the weights.
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()    
+
+        if epoch % 100 == 99:        
+            epoch_vals = str(epoch).zfill(3)
+            w = model.state_dict()['linear.weight']
+            b = model.state_dict()['linear.bias']        
+            w = ', '.join([f"{x:.8f}" for x in w.reshape(-1).tolist()])
+            b = f"{b.tolist()[0]:.6f}"
+            loss_val = f"{loss.item():.11f}"
+            print(f"|{epoch_vals: ^5}|{w: ^25}|{b: ^10}|{loss_val: ^15}|")
+    
+    # Saved your model    
+    torch.save(model.state_dict(), saved_model_name)
+
+if __name__ == "__main__":
+    n_epochs = int(input("Input the number of epochs: "))   
+    saved_model_name = input("Name to saved your model: ")
+    print(100*"=")
+    save_model(model, inputs, labels, criterion, optimizer, 
+               n_epochs=n_epochs, if_ridge=True, saved_model_name=saved_model_name)
+    
+    # if you wanna to load model
+    # model = LinearRegressionModel(2, 1)
+    # model.load_state_dict(torch.load("reg_model.h5"))
